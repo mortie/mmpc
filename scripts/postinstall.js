2383cfa20f98086b26eac85c4d367008c266b629
mort <martid0311@gmail.com>
2015-12-05 15:53:18 +0100

    mmpc now manages both remote-desktop and media-streamer

---
+++ scripts/postinstall.js
@@ -0,0 +1,19 @@
+var exec = require("child_process").spawn;
+
+function install(name) {
+	var dir = process.cwd();
+
+	process.chdir(name);
+	var child = exec("npm", ["install"]);
+	process.chdir(dir);
+
+	child.stdout.on("data", function(data) {
+		console.log(name+"(stdout): "+data.toString().trim());
+	});
+	child.stderr.on("data", function(data) {
+		console.log(name+"(stderr): "+data.toString().trim());
+	});
+}
+
+install("media-streamer");
+install("remote-desktop");
--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 var express = require("express");
 var fs = require("fs");
+var exec = require("child_process").spawn;
 
 var conf = JSON.parse(fs.readFileSync("conf.json"));
+
+function load(name) {
+	var dir = process.cwd();
+	process.chdir(name);
+
+	//Create conf entry if it doesn't exist
+	if (!conf[name]) {
+		conf[name] = JSON.parse(fs.readFileSync("conf.json.example"));
+		console.log(conf);
+	}
+
+	//Create module conf
+	fs.writeFileSync("conf.json", JSON.stringify(conf[name], null, 4));
+
+	//Init process
+	var child = exec("npm", ["start"]);
+	load.children[name] = child;
+	child.stdout.pipe(process.stdout);
+	child.stderr.pipe(process.stderr);
+
+	process.chdir(dir);
+
+	delete conf[name].host;
+	fs.writeFileSync("conf.json", JSON.stringify(conf, null, 4));
+}
+load.children = {};
+
+load("media-streamer");
+load("remote-desktop");
 
 //Static web content
 fs.writeFileSync("web/conf.js", "window.conf = "+JSON.stringify(conf)+";");
