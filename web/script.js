08b0c478249694071c94916cefbb57d6eadadd0e
mort <martid0311@gmail.com>
2015-12-10 16:23:35 +0100

    some gui improvements

--- web/script.js
+++ web/script.js
@@ -2,11 +2,31 @@ var nav = {
 	mediaStreamer: document.getElementById("nav-media-streamer"),
 	remoteDesktop: document.getElementById("nav-remote-desktop")
 };
-var frame = document.getElementById("frame");
+var frames = {
+	"media-streamer": document.getElementById("frame-media-streamer"),
+	"remote-desktop": document.getElementById("frame-remote-desktop")
+};
 
 function load(name) {
-	frame.src = "http://"+conf.host+":"+conf[name].port;
 	location.hash = name;
+
+	if (load.frame) {
+		load.frame.style.display = "none";
+
+		//Automatically unload remote desktop,
+		//to reduce server CPU usage
+		if (load.frame === frames["remote-desktop"])
+			load.frame.src = "http://false/";
+	}
+
+	var frame = frames[name];
+	load.frame = frame;
+	console.log(frame.src, !!frame.src);
+
+	if (!frame.src || frame.src === "http://false/")
+		frame.src = "http://"+conf.host+":"+conf[name].port;
+
+	frame.style.display = "block";
 }
 
 if (location.hash)
@@ -24,6 +44,6 @@ nav.remoteDesktop.addEventListener("click", function() {
 document.body.addEventListener("click", function() {
 	console.log("click");
 	setTimeout(function() {
-		frame.contentWindow.focus();
+		load.frame.contentWindow.focus();
 	}, 100);
 });
